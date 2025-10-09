579b7dc169ecc600de55493dd09d27e09e4f42f8
markhv-dev <markhv.dev@gmail.com>
2025-10-09 02:06:14 -0500

    limpieza para proyecto

--- db.js
+++
@@ -1,8 +0,0 @@
-// Conexión a la base de datos
-const config = require('./config.json');
-
-function connect() {
-    console.log(`Conectando a DB en ${config.host}:${config.port}`);
-}
-
-module.exports = { connect };
